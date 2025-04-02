3c0e1549a27912242b0531c173c47096e933fc89
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-04-01 19:45:40 -0500

    practice

--- for-each/ec2.tf
+++ for-each/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "expense" {
 for_each = var.instance_names # eack.key and eack.value
-ami = data.aws_ami.ami_info.id
+ami = data.aws_ami.ami_in.id
 vpc_security_group_ids = ["sg-010bed7b233b30d47"]
 instance_type = each.value
 tags = merge (
